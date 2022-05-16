a75505ec0bdab1caaea88e854f6e6fe1c7c592eb
Roger <roger.herling@mheducation.com>
2022-05-16 11:46:07 -0400

    init

---
+++ remove quarantine flag from k4 plugins-InCopy 2021.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/zsh
+
+xattr -r -d com.apple.quarantine /Applications/Adobe\ InCopy\ 2021/Plug-ins/movemen/
+xattr -r -d com.apple.quarantine /Applications/Adobe\ InCopy\ 2021/Plug-ins/vjoon\ K4/
+xattr -r -d com.apple.quarantine /Applications/Adobe\ InCopy\ 2021/Plug-ins/vjoon\ Plug-ins/
+
+
+exit 0
